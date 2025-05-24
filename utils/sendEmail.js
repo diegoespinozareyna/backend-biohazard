768534273b94a2922801d47b3024a1ed7b8daa4c
diegoespinozareyna <diegoespinozareyna@gmail.com>
2025-05-24 12:25:02 -0500

    fix add ruta2

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -15,7 +15,7 @@ export default async function serndEmail(base64Content, documentoCliente, emailC
         from: process.env.GMAIL_USUARIO_PROD,
         to: ["diegoespinozareyna@gmail.com", "supervisorbiohazard@gmail.com"],
         // cc: correoCliente,
-        subject: `${nombresCliente} - ${moment().tz('America/Lima').format('YYYY-MM-DD HH:mm:ss')}}`, // Asunto del correo
+        subject: `${nombresCliente} - ${moment().tz('America/Lima').format('YYYY-MM-DD HH:mm:ss')}`, // Asunto del correo
         text: `Documento generado ${moment().tz('America/Lima').format('YYYY-MM-DD HH:mm:ss')}`,
         attachments: [
             {
